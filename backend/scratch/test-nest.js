0783533c5c730c9dae52f26f911dcffe73758bc9
FelipeRinaldi1 <felipe.rinaldi.sobreira.dev0@gmail.com>
2026-08-08 23:11:29 -0300

    fix(scrach): remoção

--- backend/scratch/test-nest.js
+++
@@ -1,3 +0,0 @@
-const { NestFactory } = require('@nestjs/core');
-const { AppModule } = require('../dist/app.module'); // We need to compile or run via ts-node
-// Let's run it using ts-node directly!
